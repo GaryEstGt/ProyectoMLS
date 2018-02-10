531e332c23f89ca40794a9c6fb1b9f3d0a5bc949
GaryEstGt <31348611+GaryEstGt@users.noreply.github.com>
2018-02-10 11:45:57 -0600

    Agregando clase jugador

---
+++ Lab1MLS/Models/Jugador.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Lab1MLS.Models
+{
+    public class Jugador
+    {
+        [Key]
+        int Id { get; set; }
+        [Display(Name = "Nombre")]
+        String Name { get; set; }
+        [Display(Name = "Apellido")]
+        String LastName { get; set; }
+        [Display(Name = "Posición")]
+        String Position { get; set; }
+        [Display(Name = "Salario Base")]
+        String SalarioBase { get; set; }
+        [Display(Name = "Salario total")]
+        String SalarioTotal { get; set; }
+        [Display(Name = "Club")]
+        String Club { get; set; }
+    }
+
+}
