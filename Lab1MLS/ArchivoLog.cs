6d22ad706ba645778cffa9c7485d2d455d6b3ec1
Luis Farfán <luise.prado99@hotmail.com>
2018-02-18 22:59:21 -0600

    FinArchivo

---
+++ Lab1MLS/ArchivoLog.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.IO;
+
+namespace Lab1MLS
+{
+    public class ArchivoLog
+    {
+        static string filename;
+        public static void EmpezarLog()
+        {
+            filename = "Log.txt";            
+        }
+        public static void EscribirLinea(string Log)
+        {            
+            StreamWriter w = File.AppendText(HttpContext.Current.Server.MapPath(path: @"~\Log\" + filename));
+            w.WriteLine(Log);
+            w.WriteLine("");
+            w.Close();
+        }
+    }
+}
